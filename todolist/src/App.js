1b6be07a78748d9582f4e61e36c7722adb297716
adicp <adelajda.cupi@gmail.com>
2022-03-17 20:33:00 +0100

    test

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,11 +1,48 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import ToDoList from './Components/ToDoList';
 
 function App() {
+  const [newTask, setNewTask] = useState("");
+  const [taskArr,setTaskArr] = useState([]);
+
+  const handleList = (e) => {
+    console.log (taskArr.length);
+
+    const tasks = {
+      text: newTask,
+      completed: false
+    } 
+    setTaskArr ([...taskArr, tasks]);
+    setNewTask("");
+};
+
+const handleCompleteTask = (index) => {
+    const taskUpdated = taskArr[index];
+    console.log(taskUpdated);
+    taskUpdated.completed = !taskUpdated.completed;
+    taskArr[index]=taskUpdated;
+    console.log(taskArr);
+};
+
+const handleDeleteTask = (index) => {
+
+};
+
+
   return (
     <div className="App">
-      <ToDoList />
+        <input id="mainrow" type="text" value = { newTask } onChange = {(e) => setNewTask(e.target.value)}/>
+        <button id="mainrow" onClick={ handleList }>Add</button>
+        {
+          taskArr.map ((item, i) => 
+            <ul>
+              <li key={ i } >{item.text} 
+              <input type="checkbox" onChange={() => handleCompleteTask (i) } checked= {item.completed}></input>
+              <button onClick={ ()=> handleDeleteTask (i)}>Delete</button>
+              </li>
+            </ul>)
+        }
     </div>
   );
 }
