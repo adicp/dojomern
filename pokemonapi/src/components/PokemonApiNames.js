3a759061310d9a75093432b2e880e57047cc2667
adicp <adelajda.cupi@gmail.com>
2022-03-24 15:10:03 +0100

    fixed

--- pokemonapi/src/components/PokemonApiNames.js
+++ pokemonapi/src/components/PokemonApiNames.js
@@ -2,18 +2,24 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 const PokemonApiNames = (props) => {
-    const [people, setPeople] = useState(null);
+    const [people, setPeople] = useState([]);
 
     useEffect ( () => {
-        axios.get ('https://pokeapi.co/api/v2/pokemon?limit=807&offset=1')
-        // .then( response => response.json())
-        .then(response => setPeople(response.data))
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=807&offset=1')
+        .then(response => response.json())
+        .then(response => setPeople(response.results))
+        .catch( err => console.log(err))
     },[]);
 
     return (
         <div className='pokemonlist'>
             <ul>
-                { people }
+            {
+                people.length > 0 && people.map((item,index) => {
+                    item.name = (item.name).charAt(0).toUpperCase() + (item.name).slice(1);
+                    return (<li key = {index}>{item.name}</li>);
+                })
+            }
             </ul>
         </div>
     );
