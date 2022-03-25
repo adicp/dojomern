a1836ed449aefdb1f3d2850ebbd540e160019cd3
adicp <adelajda.cupi@gmail.com>
2022-03-25 11:22:41 +0100

    react start example

---
+++ reactrouter/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {
+  BrowserRouter,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>Routing Example</h1>
+    </BrowserRouter>
+  );
+}
+
+export default App;
